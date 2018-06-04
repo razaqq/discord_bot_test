6de283648e58ee8626fa706f002910b59ae81f31
RaZaq420 <paul.lais@web.de>
2018-06-04 19:41:57 +0200

    SQL TABLES

---
+++ databases/trump_quotes.db.sql
@@ -0,0 +1,67 @@
+BEGIN TRANSACTION;
+CREATE TABLE IF NOT EXISTS `stats` (
+	`player`	TEXT NOT NULL,
+	`points`	INTEGER
+);
+CREATE TABLE IF NOT EXISTS `quotes` (
+	`quote`	TEXT NOT NULL,
+	`answer`	TEXT NOT NULL
+);
+INSERT INTO `quotes` VALUES ('Can you imagine what the outcry would be if @SnoopDogg, failing career and all, had aimed and fired the gun at President Obama? Jail time!','True');
+INSERT INTO `quotes` VALUES ('Any negative polls are fake news, just like the CNN, ABC, NBC polls in the election. Sorry, people want border security and extreme.','True');
+INSERT INTO `quotes` VALUES ('Watched protests yesterday but was under the impression that we just had an election! Why didn’t these people vote? Celebs hurt cause badly.','True');
+INSERT INTO `quotes` VALUES ('We are going to have an unbelievable, perhaps record-setting turnout for the inauguration, and there will be plenty of movie and entertainment stars. All the dress shops are sold out in Washington. It’s hard to find a great dress for this inauguration.','True');
+INSERT INTO `quotes` VALUES ('Happy New Year to all, including to my many enemies and those who have fought me and lost so badly they just don’t know what to do. Love!','True');
+INSERT INTO `quotes` VALUES ('An ‘extremely credible source’ has called my office and told me that Barack Obama’s birth certificate is a fraud','True');
+INSERT INTO `quotes` VALUES ('Robert Pattinson should not take back Kristen Stewart. She cheated on him like a dog & will do it again – just watch. He can do much better!','True');
+INSERT INTO `quotes` VALUES ('Ariana Huffington is unattractive, both inside and out. I fully understand why her former husband left her for a man – he made a good decision.','True');
+INSERT INTO `quotes` VALUES ('Meryl Streep, one of the most over-rated actresses in Hollywood, doesn’t know me but attacked last night at the Golden Globes. She is a Hillary flunky who lost big. For the 100th time, I never “mocked” a disabled reporter (would never do that) but simply showed him “groveling” when he totally changed a 16 year old story that he had written in order to make me look bad. Just more very dishonest media!','True');
+INSERT INTO `quotes` VALUES ('I will build a great wall – and nobody builds walls better than me, believe me – and I’ll build them very inexpensively. I will build a great, great wall on our southern border, and I will make Mexico pay for that wall. Mark my words.','True');
+INSERT INTO `quotes` VALUES ('When Mexico sends its people, they’re not sending the best. They’re not sending you, they’re sending people that have lots of problems and they’re bringing those problems with us. They’re bringing drugs. They’re bring crime. They’re rapists… And some, I assume, are good people.','True');
+INSERT INTO `quotes` VALUES ('Our great African-American President hasn’t exactly had a positive impact on the thugs who are so happily and openly destroying Baltimore.','True');
+INSERT INTO `quotes` VALUES ('If I were running ‘The View’, I’d fire Rosie O’Donnell. I mean, I’d look at her right in that fat, ugly face of hers, I’d say ‘Rosie, you’re fired.','True');
+INSERT INTO `quotes` VALUES ('All of the women on The Apprentice flirted with me – consciously or unconsciously. That’s to be expected.','True');
+INSERT INTO `quotes` VALUES ('One of they key problems today is that politics is such a disgrace. Good people don’t go into government.','True');
+INSERT INTO `quotes` VALUES ('The beauty of me is that I’m very rich.','True');
+INSERT INTO `quotes` VALUES ('It’s freezing and snowing in New York – we need global warming!','True');
+INSERT INTO `quotes` VALUES ('I’ve said if Ivanka weren’t my daughter, perhaps I’d be dating her.','True');
+INSERT INTO `quotes` VALUES ('My fingers are long and beautiful, as, it has been well documented, are various other parts of my body.','True');
+INSERT INTO `quotes` VALUES ('I have never seen a thin person drinking Diet Coke.','True');
+INSERT INTO `quotes` VALUES ('I think the only difference between me and the other candidates is that I’m more honest and my women are more beautiful.','True');
+INSERT INTO `quotes` VALUES ('The point is, you can never be too greedy.','True');
+INSERT INTO `quotes` VALUES ('Sorry, there is no STAR on the stage tonight!','True');
+INSERT INTO `quotes` VALUES ('My Twitter has become so powerful that I can actually make my enemies tell the truth.','True');
+INSERT INTO `quotes` VALUES ('My IQ is one of the highest — and you all know it! Please don’t feel so stupid or insecure; it’s not your fault.','True');
+INSERT INTO `quotes` VALUES ('I have so many fabulous friends who happen to be gay, but I am a traditionalist.','True');
+INSERT INTO `quotes` VALUES ('Who wouldn’t take Kate’s picture and make lots of money if she does the nude sunbathing thing. Come on Kate!','True');
+INSERT INTO `quotes` VALUES ('The other candidates — they went in, they didn’t know the air conditioning didn’t work. They sweated like dogs…How are they gonna beat ISIS? I don’t think it’s gonna happen.','True');
+INSERT INTO `quotes` VALUES ('Look at those hands, are they small hands? And, [Republican rival Marco Rubio] referred to my hands: ‘If they’re small, something else must be small.’ I guarantee you there’s no problem. I guarantee.','True');
+INSERT INTO `quotes` VALUES ('Lyin’ Ted Cruz just used a picture of Melania from a shoot in his ad. Be careful, Lyin’ Ted, or I will spill the beans on your wife!','True');
+INSERT INTO `quotes` VALUES ('I was down there, and I watched our police and our firemen, down on 7-Eleven, down at the World Trade Center, right after it came down','True');
+INSERT INTO `quotes` VALUES ('The only card [Hillary Clinton] has is the woman’s card. She’s got nothing else to offer and frankly, if Hillary Clinton were a man, I don’t think she’d get 5 percent of the vote. The only thing she’s got going is the woman’s card, and the beautiful thing is, women don’t like her.','True');
+INSERT INTO `quotes` VALUES ('Number one, I have great respect for women. I was the one that really broke the glass ceiling on behalf of women, more than anybody in the construction industry.','True');
+INSERT INTO `quotes` VALUES ('I’m just thinking to myself right now, we should just cancel the election and just give it to Trump, right?','True');
+INSERT INTO `quotes` VALUES ('You know, it really doesn’t matter what the media write as long as you’ve got a young, and beautiful piece of ass.','True');
+INSERT INTO `quotes` VALUES ('I thought being President would be easier than my old life.','True');
+INSERT INTO `quotes` VALUES ('[North Korean leader Kim Jong Un] is 27 years old. His father dies, took over a regime. So say what you want but that is not easy, especially at that age.','True');
+INSERT INTO `quotes` VALUES ('You know, I’m automatically attracted to beautiful — I just start kissing them. It’s like a magnet. Just kiss. I don’t even wait. And when you’re a star, they let you do it. You can do anything….Grab them by the pussy. You can do anything.','True');
+INSERT INTO `quotes` VALUES ('Despite the constant negative press covfefe','True');
+INSERT INTO `quotes` VALUES ('You have a bunch of bad hombres down there. You aren’t doing enough to stop them. I think your military is scared. Our military isn’t, so I might just send them down to take care of it.','True');
+INSERT INTO `quotes` VALUES ('Yes, Arnold Schwarzenegger did a really bad job as Governor of California and even worse on the Apprentice…but at least he tried hard!','True');
+INSERT INTO `quotes` VALUES ('40 Wall Street actually was the second-tallest building in downtown Manhattan…And now it’s the tallest.','True');
+INSERT INTO `quotes` VALUES ('Why can’t we use nuclear weapons?','True');
+INSERT INTO `quotes` VALUES ('Prime Minister Abe, on behalf of the American people, I welcome you to the very famous White House.','True');
+INSERT INTO `quotes` VALUES ('[The New York Times] don’t write good. They have people over there, like Maggie Haberman and others, they don’t – they don’t write good. They don’t know how to write good.','True');
+INSERT INTO `quotes` VALUES ('I always wanted to get the Purple Heart. This was much easier.','True');
+INSERT INTO `quotes` VALUES ('I’ve had a beautiful, I’ve had a flawless campaign. You’ll be writing books about this campaign.','True');
+INSERT INTO `quotes` VALUES ('Russia, if you’re listening, I hope you’re able to find the 30,000 [Hillary Clinton] emails that are missing. I think you will probably be rewarded mightily by our press.','True');
+INSERT INTO `quotes` VALUES ('I think I am actually humble. I think I’m much more humble than you would understand.','True');
+INSERT INTO `quotes` VALUES ('Fake news is at an all time high. Where is their apology to me for all of the incorrect stories???','True');
+INSERT INTO `quotes` VALUES ('You could see there was blood coming out of her eyes, blood coming out of her wherever.','True');
+INSERT INTO `quotes` VALUES ('I know more about ISIS than the generals do. Believe me.','True');
+INSERT INTO `quotes` VALUES ('My use of social media is not presidential – it’s MODERN DAY PRESIDENTIAL. Make America Great Again!','True');
+INSERT INTO `quotes` VALUES ('I heard poorly rated @Morning_Joe speaks badly of me (don’t watch anymore). Then how come low I.Q. Crazy Mika, along with Psycho Joe came to Mar-a-Lago 3 nights in a row around New Year’s Even, and insisted on joining me. She was bleeding badly from a face-lift. I said no!','True');
+INSERT INTO `quotes` VALUES ('Most politicians would have gone to a meeting like the one Don jr attended in order to get info on an opponent. That’s politics!','True');
+INSERT INTO `quotes` VALUES ('Eventually we’re going to get something done and it’s going to be really, really good.','True');
+INSERT INTO `quotes` VALUES ('Why would Kim Jong-un insult me by calling me `old`, when I would NEVER call him `short and fat?` Oh well, I try so hard to be his friend - and maybe someday that will happen!','True');
+COMMIT;
