b9b35d66ef3550fe090414647eced1a13566982b
razaqq <paul.lais@web.de>
2019-03-09 15:08:23 +0100

    made the stream run on a new thread

--- ext/trump_twitter.py
+++ ext/trump_twitter.py
@@ -1,134 +1,98 @@
-import discord
-
-import asyncio
-import json
+from twitter import TwitterStream, OAuth
+from dataclasses import dataclass
 import logging
-
-from tweepy import OAuthHandler
-from tweepy import Stream
-from tweepy import API
-from tweepy.streaming import StreamListener
-
-tweets = []
+import discord
+import json
 
 
-class TrumpTwitter:
-    def __init__(self, bot):
-        self.bot = bot
-        self.config = self.load_config(self.bot.root_dir)
-
-    @staticmethod
-    def load_config(root_dir):
-        with open(root_dir + '/config/trump_twitter.json', 'r', encoding='utf-8') as doc:
-            return json.load(doc)
-
-    def start_stream(self):
-        auth = OAuthHandler(self.config['consumer_key'], self.config['consumer_secret'])
-        auth.set_access_token(self.config['access_token'], self.config['access_secret'])
-        api = API(auth)
-
-        listener = StdOutListener()
-
-        stream = Stream(auth=api.auth, listener=listener)
-        stream.filter(follow=['25073877'], is_async=True)
-        # stream.filter(track=['sandwich'], is_async=True)
-
-    async def check_tweets(self):
-        while self.bot.get_cog('TrumpTwitter'):  # check this again
-            try:
-                tweet_amount = len(tweets)
-                i = 0
-                while i < tweet_amount:
-                    tweet = tweets[i]
-
-                    all_servers = self.bot.servers
-                    server = discord.utils.get(all_servers, id=self.config['server_id'])
-                    channel = server.get_channel(self.config['channel_id'])
-
-                    url = 'https://twitter.com/statuses/{}'.format(tweet.id)
-
-                    embed = discord.Embed(description="{}\n\n[Link]({})".format(tweet.text, url), color=0x00aced)
-                    embed.set_author(name=tweet.name, url='https://twitter.com/realDonaldTrump', icon_url=tweet.avatar)
-                    embed.set_footer(text=tweet.time, icon_url='https://abs.twimg.com/icons/apple-touch-icon-192x192.png')
-                    embed.set_thumbnail(url='https://abs.twimg.com/icons/apple-touch-icon-192x192.png')
-
-                    if tweet.image:
-                        embed.set_image(url=tweet.image)
-
-                    await self.bot.send_message(channel, embed=embed)
-
-                    tweet_amount -= 1
-                    del tweets[i]
-
-            except Exception as e:
-                logging.log(20, e)
-
-            await asyncio.sleep(60)
-
-
+@dataclass
 class Tweet:
-    def __init__(self, id_str, name, time, text, avatar, image):
-        self.id = id_str
-        self.name = name
-        self.time = time
-        self.text = text
-        self.avatar = avatar
-        self.image = image
+    id: str
+    name: str
+    time: str
+    text: str
+    avatar: str
+    image: str
 
     def __str__(self):
         s = '{} | {}: "{}"'.format(self.time, self.name, self.text)
         return s
 
 
-class StdOutListener(StreamListener):
-    def on_connect(self):
-        logging.info("Successfully connected to the twitter api.")
+class TrumpTwitter:
+    def __init__(self, bot):
+        self.bot = bot
+        self.config = self.load_config(self.bot.root_dir)
+        self.tweet_iter = self.setup()
 
-    def on_status(self, status):
-        if status.user.id != 25073877:  # from trump?
-            return
-        if hasattr(status, 'retweeted_status'):  # fuck retweets
-            return
+    @staticmethod
+    def load_config(root_dir):
+        with open(root_dir + '/config/trump_twitter.json', 'r', encoding='utf-8') as doc:
+            return json.load(doc)
 
-        if not status.truncated:
-            text = status.text
-            image = None
-            if 'media' in status.entities:
-                if status.entities['media'][0]['type'] == 'photo':  # only jpeg, no gif/video
-                    image = status.entities['media'][0]['media_url_https']
-                    media_url = status.entities['media'][0]['url']  # ex https://t.co/ywU2CEih8b
-                    if text.endswith(media_url):  # remove media url from tweet text
-                        url_lenght = len(media_url) + 1  # remove the whitespace aswell
-                        text = text[:-url_lenght]
-        else:
-            text = status.extended_tweet['full_text']
-            image = None
-            if 'media' in status.extended_tweet['entities']:
-                if status.extended_tweet['entities']['media'][0]['type'] == 'photo':  # only jpeg, no gif/video
-                    image = status.extended_tweet['entities']['media'][0]['media_url_https']
-                    media_url = status.extended_tweet['entities']['media'][0]['url']  # ex https://t.co/ywU2CEih8b
-                    if text.endswith(media_url):  # remove media url from tweet text
-                        url_lenght = len(media_url) + 1  # remove the whitespace aswell
-                        text = text[:-url_lenght]
+    def setup(self):
+        auth = OAuth(self.config['access_token'],
+                     self.config['access_secret'],
+                     self.config['consumer_key'],
+                     self.config['consumer_secret'])
+        stream = TwitterStream(auth=auth, secure=True)
+        return stream.statuses.filter(follow='25073877')
 
-        tweet = Tweet(status.id_str, status.user.name, status.created_at, text, status.user.profile_image_url_https, image)
-        tweets.append(tweet)
+    async def read_tweets(self):
+        for tweet in self.tweet_iter:
+            if tweet['user']['id'] != 25073877:  # skip tweets that aren't from god himself
+                continue
+            if 'retweeted_status' in tweet or tweet['text'].startswith('RT @'):  # filter retweets
+                continue
+            if tweet['truncated']:  # check if tweet is truncated
+                image = None
+                text = tweet['extended_tweet']['full_text']
+                if 'media' in tweet['extended_tweet']['entities']:
+                    if tweet['extended_tweet']['entities']['media'][0]['type'] == 'photo':  # only jpeg, no gif/video
+                        image = tweet['extended_tweet']['entities']['media'][0]['media_url_https']
+                        media_url = tweet['extended_tweet']['entities']['media'][0]['url']  # ex https://t.co/ywU2CEih8b
+                        if text.endswith(media_url):  # remove media url from tweet text
+                            url_lenght = len(media_url) + 1  # remove the whitespace aswell
+                            text = text[:-url_lenght]
+            else:
+                image = None
+                text = tweet['text']
+                if 'media' in tweet['entities']:
+                    if tweet['entities']['media'][0]['type'] == 'photo':  # only jpeg, no gif/video
+                        image = tweet['entities']['media'][0]['media_url_https']
+                        media_url = tweet['entities']['media'][0]['url']  # ex https://t.co/ywU2CEih8b
+                        if text.endswith(media_url):  # remove media url from tweet text
+                            url_lenght = len(media_url) + 1  # remove the whitespace aswell
+                            text = text[:-url_lenght]
 
-        tweet = str(tweet).encode('utf-8')
-        logging.info(tweet)
+            tweet = Tweet(tweet['id_str'],
+                          tweet['user']['name'],
+                          tweet['created_at'],
+                          text,
+                          tweet['user']['profile_image_url_https'],
+                          image)
 
-    def on_error(self, error_code):
-        if error_code == 420:
-            logging.error("420 Enhance Your Calm - We are being rate limited."
-                          "Possible reasons: Too many login attempts or running too many copies of the same "
-                          "application authenticating with the same credentials")
-            return False  # returning False in on_error disconnects the stream
-        logging.error(f"Error: {error_code}")
-        return True
+            await self.post_tweet(tweet)
+
+    async def post_tweet(self, tweet):
+        all_servers = self.bot.servers
+        server = discord.utils.get(all_servers, id=self.config['server_id'])
+        channel = server.get_channel(self.config['channel_id'])
+
+        url = 'https://twitter.com/statuses/{}'.format(tweet.id)
+
+        embed = discord.Embed(description="{}\n\n[Link]({})".format(tweet.text, url), color=0x00aced)
+        embed.set_author(name=tweet.name, url='https://twitter.com/realDonaldTrump', icon_url=tweet.avatar)
+        embed.set_footer(text=tweet.time, icon_url='https://abs.twimg.com/icons/apple-touch-icon-192x192.png')
+        embed.set_thumbnail(url='https://abs.twimg.com/icons/apple-touch-icon-192x192.png')
+
+        if tweet.image:
+            embed.set_image(url=tweet.image)
+
+        await self.bot.send_message(channel, embed=embed)
 
 
 def setup(bot):
-    t = TrumpTwitter(bot)
-    t.start_stream()
-    bot.loop.create_task(t.check_tweets())
-    bot.add_cog(TrumpTwitter(bot))
+    tw = TrumpTwitter(bot)
+    bot.loop.create_task(tw.read_tweets())
+    bot.add_cog(tw)
