7af0bfb487f8a415ca5eea2f68ac8b349ddb22c5
razaqq <paul.lais@web.de>
2019-03-11 01:11:34 +0100

    fixed jacobs garbage coding

--- ext/fake_news.py
+++ ext/fake_news.py
@@ -4,7 +4,7 @@ import json
 import discord
 
 
-class SlideIntoDMs:
+class FakeNews:
     def __init__(self, bot):
         self.bot = bot
         self.prefix = self.bot.config['prefix']
@@ -16,30 +16,30 @@ class SlideIntoDMs:
             return json.load(doc)
 
     @commands.command(pass_context=True, hidden=True)
-    async def dm(self, ctx, text=None):
+    async def fake(self, ctx, *, text=None):
         msg = ctx.message
-        role_mentions = msg.role_mentions
-        user_mentions = msg.mentions
-        
         if msg.channel.is_private and msg.author != self.bot.user and text:
             await self.bot.say("Preparing your tweet...")
             # in private channel with msg.author
             all_servers = self.bot.servers
             server = discord.utils.get(all_servers, id=self.config['server_id'])
             channel = server.get_channel(self.config['channel_id'])
-            await self.sendFakeNews(text, channel)
+            await self.send_fake_news(text, channel)
         else:
             await self.bot.say("Git gud")
 
-    async def sendFakeNews(self, text, channel):
+    async def send_fake_news(self, text, channel):
         now = datetime.datetime.now()
+        now = now.strftime("%a %b %d %H:%M:%S +0000 %Y")
         embed = discord.Embed(description="{}\n\n[Link]({})".format(text, "http://perryswift.tk"), color=0x00aced)
-        embed.set_author(name="Donald J. Trump", url='https://twitter.com/realDonaldTrump', icon_url="https://pbs.twimg.com/profile_images/874276197357596672/kUuht00m_normal.jpg")
-        embed.set_footer(text=now.strftime("%Y-%m-%d"), icon_url='https://abs.twimg.com/icons/apple-touch-icon-192x192.png')
+        embed.set_author(name="Donald J. Trump", url='https://twitter.com/realDonaldTrump',
+                         icon_url="https://pbs.twimg.com/profile_images/874276197357596672/kUuht00m_normal.jpg")
+        embed.set_footer(text=now,
+                         icon_url='https://abs.twimg.com/icons/apple-touch-icon-192x192.png')
         embed.set_thumbnail(url='https://abs.twimg.com/icons/apple-touch-icon-192x192.png')
 
         await self.bot.send_message(channel, embed=embed)
 
 
 def setup(bot):
-    bot.add_cog(SlideIntoDMs(bot))
+    bot.add_cog(FakeNews(bot))
