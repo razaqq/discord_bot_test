e9a567b77215c5d7ed2f0bcfb9e1c80c6e7f4e85
razaqq <paul.lais@web.de>
2020-01-08 19:47:22 +0100

    edited formatting, switched to f-strings

--- ext/admin.py
+++ ext/admin.py
@@ -41,7 +41,7 @@ class Admin(commands.Cog):
             t.align = "r"
             t.field_names = ['Extension', 'Status']
             t.align = "r"
-            await ctx.send('```{}```'.format(t.get_string(sortby="Extension")))
+            await ctx.send(f'```{t.get_string(sortby="Extension")}```')
 
     @exts.command(pass_context=True, hidden=True)
     async def enable(self, ctx, module=None):
@@ -51,21 +51,21 @@ class Admin(commands.Cog):
             if not module:
                 return await ctx.send('Please specify a module')
             try:
-                self.bot.load_extension('ext.{}'.format(module))
+                self.bot.load_extension(f'ext.{module}')
                 self.main_config.enabled_exts.append(str(module))
                 self.bot.config.save()
             except commands.ExtensionNotFound:
-                return await ctx.send("There is no ext with that name")
+                return await ctx.send('There is no ext with that name')
             except commands.ExtensionAlreadyLoaded:
-                return await ctx.send("This ext is already enabled")
+                return await ctx.send('This ext is already enabled')
             except commands.NoEntryPointError:
                 return await ctx.send('That ext has no setup function')
             except commands.ExtensionFailed as e:
-                await ctx.send('Something went wrong when I tried to enable that ext:')
-                return await ctx.send('```py\n{}\n```'.format(traceback.format_exc()))
+                await ctx.send('Something went wrong trying to enable ext:')
+                return await ctx.send(f'```py\n{traceback.format_exc()}\n```')
             else:
                 await ctx.send('\N{OK HAND SIGN}')
-                logging.log(20, 'Ext "{}" was successfully enabled by {}'.format(module, author.name))
+                logging.log(20, f'Ext "{module}" was successfully enabled by {author.name}')
         else:
             await ctx.send("You don't have permissions")
 
@@ -77,14 +77,14 @@ class Admin(commands.Cog):
             if not module:
                 return await ctx.send('Please specify a module')
             try:
-                self.bot.unload_extension('ext.{}'.format(module))
+                self.bot.unload_extension(f'ext.{module}')
                 self.main_config.enabled_exts.remove(str(module))
                 self.bot.config.save()
             except commands.ExtensionNotLoaded:
                 return await ctx.send('There is no ext enabled with that name')
             else:
                 await ctx.send('\N{OK HAND SIGN}')
-                logging.log(20, 'Ext "{}" was successfully disabled by {}'.format(module, author.name))
+                logging.log(20, f'Ext "{module}" was successfully disabled by {author.name}')
         else:
             await ctx.send("You don't have permissions")
 
@@ -96,19 +96,19 @@ class Admin(commands.Cog):
             if not module:
                 return await ctx.send('Please specify a module')
             try:
-                self.bot.reload_extension('ext.{}'.format(module))
+                self.bot.reload_extension(f'ext.{module}')
             except commands.ExtensionNotLoaded:
                 return await ctx.send('There is no ext enabled with that name')
             except commands.ExtensionNotFound:
-                return await ctx.send("There is no ext with that name")
+                return await ctx.send('There is no ext with that name')
             except commands.NoEntryPointError:
                 return await ctx.send('That ext has no setup function')
             except commands.ExtensionFailed as e:
-                await ctx.send('Something went wrong when I tried to enable that ext:')
-                await ctx.send('```py\n{}\n```'.format(traceback.format_exc()))
+                await ctx.send('Something went wrong trying to enable ext:')
+                await ctx.send(f'```py\n{traceback.format_exc()}\n```')
             else:
                 await ctx.send('\N{OK HAND SIGN}')
-                logging.log(20, 'Ext {} was successfully reloaded by {}'.format(module, author.name))
+                logging.log(20, f'Ext {module} was successfully reloaded by {author.name}')
         else:
             await ctx.send("You don't have permissions")
 
@@ -118,7 +118,7 @@ class Admin(commands.Cog):
         author = ctx.message.author
         if self.is_admin(author):
             await ctx.send('Shutdown pending... (trying to kill all running tasks)')
-            logging.log(20, 'SHUTDOWN ORDERED BY {}'.format(author.name))
+            logging.log(20, f'Shutdown ordered by {author.name}')
             await self.bot.shutdown(restart=False)
         else:
             await ctx.send("You don't have permissions")
@@ -129,7 +129,7 @@ class Admin(commands.Cog):
         author = ctx.message.author
         if self.is_admin(author):
             await ctx.send('Restart pending... (trying to kill all running tasks)')
-            logging.log(20, 'RESTART ORDERED BY {}'.format(author.name))
+            logging.log(20, f'Restart ordered by {author.name}')
             await self.bot.shutdown(restart=True)
         else:
             await ctx.send("You don't have permissions")
@@ -146,9 +146,9 @@ class Admin(commands.Cog):
         try:
             await self.bot.user.edit(username=name.strip())
             await ctx.send('\N{OK HAND SIGN}')
-            logging.info('Nickname changed to {} by {}')
+            logging.info(f'Nickname changed to "{name}" by {ctx.message.author.name}')
         except discord.HTTPException as e:
-            await ctx.send('Failed to rename: {}'.format(e))
+            await ctx.send(f'Failed to rename: {e}')
             logging.error(traceback.format_exc())
         except discord.ClientException:
             await ctx.send('Password required')
@@ -161,13 +161,13 @@ class Admin(commands.Cog):
             await self.bot.user.edit(avatar=response.content)
             await ctx.send('\N{OK HAND SIGN}')
         except discord.HTTPException as e:
-            await ctx.send('Failed change avatar: {}'.format(e))
+            await ctx.send(f'Failed change avatar: {e}')
             logging.error(traceback.format_exc())
         except discord.InvalidArgument:
-            await ctx.send('Wrong image format, allowed: JPEG & PNG')
+            await ctx.send('Wrong image format, allowed: `.jpg` and `.png`')
             logging.error(traceback.format_exc())
         except discord.ClientException:
-            await ctx.send('password required')
+            await ctx.send('Password required')
             logging.error(traceback.format_exc())
 
     @presence.command(pass_context=True, enabled=False, hidden=True, no_pm=True)
@@ -177,10 +177,10 @@ class Admin(commands.Cog):
             if game:
                 game = game.strip()
                 await self.bot.change_presence(game=discord.Game(name=game))
-                logging.log(20, 'Status set to "{}" by owner'.format(game))
+                logging.log(20, f'Status set to "{game}" by owner')
             else:
                 await self.bot.change_presence(game=None)
-                logging.log(20, 'status cleared by {}'.format(author.name))
+                logging.log(20, f'Status cleared by {author.name}')
                 await ctx.send('\N{OK HAND SIGN}')
         else:
             await ctx.send("You don't have permissions")
